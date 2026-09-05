fe755cce822c76296ada90473dfb833fa7eb7b94
karishmas0 <karishma05cs@gmail.com>
2026-09-05 17:30:11 +0530

    class and object basics done

---
+++ oops1.py
@@ -0,0 +1,25 @@
+# intitiate a class
+class employee:
+    # special methord/magic method/dunder method- constructor
+    def __init__(self):
+        print("started executing attributes/data")
+        self.id= 123
+        self.salary= 50000
+        self.designation="SDE"
+        print("attributes/data have been initiated")
+
+    def travel(self,destination):
+        print("this travel func was called manually")
+        print(f"Employee is now travelling to {destination}")
+
+# #create an object/instance of the class
+sam= employee()
+# print(sam.id)
+# print(sam.salary)
+# print(sam.designation)
+
+# #calling a method
+# #function ko hamesha call karna pdta hai means method ko
+# sam.travel("kerala")
+
+print(type(sam))
---
+++ rough.py
@@ -0,0 +1,15 @@
+lst=[1,2,3]
+my_str="mlops playlist"
+my_int=155
+
+# print(type(lst))
+# lst.clear()
+# print(lst)
+
+# lst.capitalize()
+# #my_str=my_str.capitalize
+# print(lst)
+
+a='x'
+b='y'
+print(a+b)# output xy
